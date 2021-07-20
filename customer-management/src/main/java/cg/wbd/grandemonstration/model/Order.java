8acdfe7f3c47c8b7b481f78cc8a6e72aea12eb7c
tiennv0608 <tiennv0608@gmail.com>
2021-07-20 15:05:28 +0700

    test tạo bảng manytoone

---
+++ customer-management/src/main/java/cg/wbd/grandemonstration/model/Order.java
@@ -0,0 +1,31 @@
+package cg.wbd.grandemonstration.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
